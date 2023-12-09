78590eed52812d9a3b918809afc847c787f79d0c
Ricardo Guardiola <ricardoguardiolahn@gmail.com>
2023-12-08 19:28:40 -0600

    setting up

---
+++ src/main/java/hn/unah/reposicion/ReposicionApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.reposicion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposicionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposicionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/unah/reposicion/ReposicionApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.reposicion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposicionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
